541ab5651ba6fbb3bf5e78b253549a0d751fc152
SalmaYousry01 <syamh26701@gmail.com>
2024-04-19 19:58:37 +0200

    navbar added

---
+++ src/app/Components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+// navbar.component.ts
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  
+}
