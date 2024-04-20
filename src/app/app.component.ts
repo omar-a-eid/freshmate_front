5ec9bec36c52b8afb94228657662d22779aecfd9
Esraa Ahmed <esraaeliba@gmail.com>
2024-04-20 23:06:37 +0200

    updating app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
 
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './Components/product/product.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, HttpClientModule, NavbarComponent, FooterComponent, ProductComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
