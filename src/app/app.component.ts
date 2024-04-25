f42472923efe6ba67bd8210c08fbb2212236728b
karim Mohamed <karimdesouki147@gmail.com>
2024-04-25 21:50:00 +0200

    validation and responsive in contactus page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ContactUSComponent } from './components/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProfileComponent,ContactUSComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
