a0bdeca10ee8a1abdf602885208dbb18170647d7
Adel <ahmeddadel9988@gmail.com>
2024-04-26 17:56:44 +0300

    not found page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ContactUSComponent } from './components/contact-us/contact-us.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProfileComponent,ContactUSComponent],
+  imports: [RouterOutlet,ProfileComponent,ContactUSComponent,NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
