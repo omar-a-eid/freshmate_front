83bb7515076441967ea499affef4e49fc9c8d8f4
Esraa Ahmed <esraaeliba@gmail.com>
2024-04-27 14:31:07 +0300

    adding faq page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import {WishlistComponent} from './Components/wishlist/wishlist.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { FaqComponent } from './Components/faq/faq.component';
 
 
 
 export const routes: Routes = [
     { path: 'wishlist', component: WishlistComponent },
     { path: 'aboutus', component: AboutUsComponent },
+    { path: 'faq', component: FaqComponent  },
+
 ];
