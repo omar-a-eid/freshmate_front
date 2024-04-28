bf7719151c2b5b203ebcc56701c35cdefe1389fd
Esraa Ahmed <esraaeliba@gmail.com>
2024-04-28 16:53:12 +0300

    adding pages routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
-import { Routes } from '@angular/router';
-import {WishlistComponent} from './Components/wishlist/wishlist.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WishlistComponent } from './Components/wishlist/wishlist.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { FaqComponent } from './Components/faq/faq.component';
-
-
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 
 export const routes: Routes = [
-    { path: 'wishlist', component: WishlistComponent },
-    { path: 'aboutus', component: AboutUsComponent },
-    { path: 'faq', component: FaqComponent  },
-
-];
+    // {path: "", redirectTo:"students", pathMatch:"full"},
+    {path: "wishlist", component: WishlistComponent},
+    {path: "aboutus", component: AboutUsComponent},
+    {path: "product", component: ProductDetailsComponent},
+    {path: "faq", component: FaqComponent}
+   
+   ];
