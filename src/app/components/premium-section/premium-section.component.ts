13c2d1e8396ccc816f5f210786e870659fc056e5
omar-a-eid <omerx9988@gmail.com>
2024-04-30 22:45:42 +0300

    Home page

--- src/app/components/premium-section/premium-section.component.ts
+++ src/app/components/premium-section/premium-section.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './premium-section.component.css'
 })
 export class PremiumSectionComponent {
-
+  items = [{text1: "Altitude", text2: "Up to ca. 800M", image: "assets/icon-b1.webp"}, {text1: "Temperature", text2:"22 - 30° C", image: "assets/icon-b3.webp"}, {text1:"Caffeine", text2:"2 - 4,5 %", image:"assets/icon-b2.webp"}, {text1:"Production", text2:"30% of the world market", image:"assets/icon-b4.webp"}]
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BenefitsSectionComponent } from '../../components/benefits-section/benefits-section.component';
+import { BestSellerComponent } from '../../components/best-seller/best-seller.component';
 import { CollectionSectionComponent } from '../../components/collection-section/collection-section.component';
 import { FeedbackSectionComponent } from '../../components/feedback-section/feedback-section.component';
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
@@ -10,7 +11,7 @@ import { PremiumSectionComponent } from '../../components/premium-section/premiu
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSectionComponent, OffersSectionComponent, BenefitsSectionComponent, PremiumSectionComponent, CollectionSectionComponent, FeedbackSectionComponent, LogosSectionComponent],
+  imports: [HeroSectionComponent, OffersSectionComponent, BenefitsSectionComponent, PremiumSectionComponent, CollectionSectionComponent, FeedbackSectionComponent, LogosSectionComponent, BestSellerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
