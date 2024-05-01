d12464a733669ab9ff3f1c23da07ddf199344a6e
omar-a-eid <omerx9988@gmail.com>
2024-05-01 04:58:36 +0300

    fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,22 @@
 import { Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ContactUSComponent } from './components/contact-us/contact-us.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductsPageComponent } from './components/products-page/products-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ContactUSComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "wishlist", component: WishlistComponent},
     {path: "aboutus", component: AboutUsComponent},
+    {path: "products", component: ProductsPageComponent},
     {path: "product/:id", component: ProductDetailsComponent},
     {path: "faq", component: FaqComponent},
     {path: 'cart', component: CartComponent},
